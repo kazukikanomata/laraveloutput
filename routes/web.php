44b191c4597aab2805bf62e8612711145d1a94da
kazukikanomata <kazu.surface.pro2020@outlook.jp>
2022-04-01 19:05:37 +0900

    ログイン中のユーザーだけが確認可能

--- routes/web.php
+++ routes/web.php
@@ -15,19 +15,23 @@ Route::get('/','TopController@index');
 
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
+
 // バナーが表示されているページ
-Route::get('/categories','CategoryController@index')->name('categories.index');
-// 選択されたカテゴリーが表示されているページ
-Route::get('/categories/{category}','CategoryController@show')->name('categories.show');
-// タスク作成画面
-Route::get('/tasks/create','TaskController@create')->name('tasks.create');
-// タスク保存画面
-Route::post('/tasks','TaskController@store')->name('tasks.store');
-//　選択されたカテゴリーの個別詳細ページ
-Route::get('/categories/{category}/tasks/{task}','TaskController@show')->name('tasks.show');
-// タスク編集
-Route::get('/tasks/{task}/edit','TaskController@edit')->name('tasks.edit');
-// タスク更新
-Route::put('/tasks/{task}','TaskController@update')->name('tasks.update');
-// タスク削除
-Route::delete('/tasks/{task}', 'TaskController@destory')->name('tasks.destory');
+Route::group(['middleware'=>'auth'], function(){
+
+    Route::get('/categories','CategoryController@index')->name('categories.index');
+    // 選択されたカテゴリーが表示されているページ
+    Route::get('/categories/{category}','CategoryController@show')->name('categories.show');
+    // タスク作成画面
+    Route::get('/tasks/create','TaskController@create')->name('tasks.create');
+    // タスク保存画面
+    Route::post('/tasks','TaskController@store')->name('tasks.store');
+    //　選択されたカテゴリーの個別詳細ページ
+    Route::get('/categories/{category}/tasks/{task}','TaskController@show')->name('tasks.show');
+    // タスク編集
+    Route::get('/tasks/{task}/edit','TaskController@edit')->name('tasks.edit');
+    // タスク更新
+    Route::put('/tasks/{task}','TaskController@update')->name('tasks.update');
+    // タスク削除
+    Route::delete('/tasks/{task}', 'TaskController@destory')->name('tasks.destory');
+});
